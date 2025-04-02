c199c7640c30a4ec15fbd9ff99f178cc0f7512f1
MrIceRam <skyfarliv@gmail.com>
2025-04-02 21:20:00 +0300

    first try

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+
+const int fld_width = 20;
+const int fld_height = 30;
+const int scr_width = fld_width * 2;
+const int scr_height = fld_height;
+
+using namespace std;
+
+int main()
+{
+	cout << "hi\n";
+	return 0;
+}
