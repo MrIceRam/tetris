f6fa034f5d056b0a1baecbae825eb7dfe377fa22
MrIceRam <skyfarliv@gmail.com>
2025-04-03 22:39:40 +0300

    first try VSC

--- tetris/tetris/tetris.cpp
+++ tetris/tetris/tetris.cpp
@@ -1,31 +1,81 @@
 ﻿#include <iostream>
-
+#include <windows.h>
+#include <conio.h>
 using namespace std;
-int x;
-int a;
-int ym2(){
-	system("CD");
-	system("DIR");
-	system("md ямамин лох лазурный");
-	return 52;
+
+void gotoxy(int x, int y) {
+	COORD p = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
 }
+
+class Game {
+public:
+	Game() {
+	}
+	void showField() {
+		for (int i = 0; i < 15; i++) {
+			cout << "#" << endl;
+		}
+		for (int i = 0; i < 20; i++) {
+			cout << "#";
+		}
+		for (int i = 0; i < 15; i++) {
+			gotoxy(19, i);
+			cout << "#" << endl;
+		}
+	}
+
+	int run() {
+		showField();
+
+		int ch = 0;
+		int i = 0;
+
+		int x = 5, y = 5;
+
+		while (true){
+			while (!_kbhit()) {
+				gotoxy(x, y);
+				cout << i % 10;
+				i++;
+			}
+			ch = _getch();
+
+			if (ch == 224) {
+				ch = _getch();
+				gotoxy(20, 20);
+				cout << ch;
+			}
+
+			switch (ch) {
+			case 77: // вправо
+				x++;
+				break;
+			}
+
+			switch (ch) {
+			case 80: // вниз
+				y++;
+				break;
+			}
+			switch (ch) {
+			case 75: // Влево
+				x--;
+				break;
+			}
+			//72 вверх
+
+
+		}
+		return 0;
+	}
+};
 
 int main()
 {
+	//system("mode con cols=20 lines=16");
 	setlocale(LC_ALL, "Russian"); //Включает Русский язык в С++
-	
-	system("mode con cols=50 lines=40");
-	system("COLOR 2");
-	cout << "хуй бобра";
-	system("CLS");
-	cin >> x;
-	if (x== 42){
-		a = ym2();
-	}
-
-	cin >> x;
-	if (a == 52){
-		system("RD ямамин лох лазурный");
-	}
-	return 0;
+	Game game;
+	game.run();
+	_getch();
 }
